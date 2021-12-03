74a12e3de3c5bd8730d2d2d0e1df5c639c044acb
zca112 <ziyue_cheng@sfu.ca>
2021-12-03 11:22:25 -0800

    bash file to run all python file that can get the final clearning data

---
+++ src/clearing_data.sh
@@ -0,0 +1,27 @@
+echo "Please enter the path you of all your python code(e.g. /Users/airbnb/src)"
+read path
+
+echo "Please enter the path you of all your dataset(e.g. /Users/airbnb/datasets)"
+read data_path
+
+echo "where you run this code? 1 for cluster and 2 for local computer"
+read var
+if [[ $var -eq 2 ]];
+then
+	${SPARK_HOME}/bin/spark-submit ${path}/osm_04_extract_features.py ${data_path}/osm_vancouver_data.json ${data_path}/osm_features
+	${SPARK_HOME}/bin/spark-submit ${path}/calc_osm_airbnb_distances.py ${data_path}/osm_features/ ${data_path}/listings.csv ${data_path}/distances
+	${SPARK_HOME}/bin/spark-submit ${path}/listing_data_clearing.py ${data_path}/listings.csv ${data_path}/calendar.csv ${data_path}/distances ${data_path}/airbnb_out
+	
+else
+	spark-submit ${path}/osm_04_extract_features.py ${data_path}/osm_vancouver_data.json ${data_path}/osm_features
+	spark-submit ${path}/calc_osm_airbnb_distances.py ${data_path}/osm_features/ ${data_path}/listings.csv ${data_path}/distances
+	spark-submit ${path}/listing_data_clearing.py ${data_path}/listings.csv ${data_path}/calendar.csv ${data_path}/distances ${data_path}/airbnb_out
+
+fi
+
+rm -r ${data_path}/distances
+rm -r ${data_path}/osm_features
+
+echo "Your final clearing data saved at ${data_path}/airbnb_out"
+#/Users/jennifer/Downloads/2021fall/732/project
+#/Users/jennifer/Downloads/2021fall/732/project/calendar
