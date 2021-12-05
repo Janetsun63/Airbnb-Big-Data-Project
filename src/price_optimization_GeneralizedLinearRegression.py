ffc218abe5dc85bf74f295dd33f289389049b35f
William <haonanw@sfu.ca>
2021-12-04 20:24:36 -0800

    Model training code

--- src/price_optimization_GeneralizedLinearRegression.py
+++ src/price_optimization_GeneralizedLinearRegression.py
@@ -6,8 +6,9 @@ from pyspark.sql import SparkSession, functions, types, Row
 
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import StringIndexer, VectorAssembler, SQLTransformer
-from pyspark.ml.regression import DecisionTreeRegressor, GBTRegressor
+import pyspark.ml.regression as regression
 from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 
 training_data_schema = types.StructType([
     types.StructField('id', types.LongType()), 
@@ -36,8 +37,6 @@ def main(inputs, output):
         schema = training_data_schema
     )
 
-    data = data.na.fill(value = 0, subset = ['beds'])
-
     train, validation = data.randomSplit([0.8, 0.2])
     train = train.cache()
     validation = validation.cache()
@@ -62,14 +61,23 @@ def main(inputs, output):
         outputCol = 'features'
     )
 
-    regressor = GBTRegressor(
+    # GeneralizedLinearRegression
+    regressor = regression.GeneralizedLinearRegression(
         featuresCol = 'features', 
         labelCol = label_column, 
         predictionCol = 'prediction', 
-        maxDepth = len(input_column_lst)
+        family = "gaussian", 
+        link = "identity", 
+        maxIter = 50, 
+        regParam = 0.2
     )
 
-    print(input_column_lst)
+    # Hyperparameters for GeneralizedLinearRegression
+    paramGrid = ParamGridBuilder() \
+        .addGrid(regressor.maxIter, range(0, 51, 5)) \
+        .addGrid(regressor.regParam, [1e-5, 1e-4, 1e-3, 1e-2, 0.1, 0.15, 0.2, 0.3]) \
+        .build()
+
 
     # Pipeline
     pipeline = Pipeline(stages = word_indexer_lst + [
@@ -77,24 +85,44 @@ def main(inputs, output):
         regressor
     ])
 
-    # Training
-    model = pipeline.fit(train)
-    predictions = model.transform(validation)
-    predictions.cache()
-
-    # Select example rows to display.
-    predictions.show(5)
-
-    # Evaluation
+    # Define evaluator
     rmse_evaluator = RegressionEvaluator(
         labelCol = label_column, 
         predictionCol = "prediction", 
         metricName = 'rmse'
     )
-    rmse = rmse_evaluator.evaluate(predictions)
 
-    print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
+    crossval = CrossValidator(
+        estimator = pipeline, 
+        estimatorParamMaps = paramGrid, 
+        evaluator = rmse_evaluator, 
+        numFolds = 5, 
+        parallelism = 8
+    )
 
+    # Training
+    cvModel = crossval.fit(train)
+
+    # Get optimal model
+    best_model = cvModel.bestModel
+    # print(best_model.stages[-1].extractParamMap())
+    print("Best Param (maxIter): %g" % best_model.stages[-1].getMaxIter())
+    print("Best Param (regParam): %d" % best_model.stages[-1].getRegParam())
+
+    # Make predictions
+    predictions = best_model.transform(validation)
+    predictions.cache()
+
+    # # Select example rows to display.
+    # predictions.show(5)
+
+    # Testing error
+    test_rmse = rmse_evaluator.evaluate(predictions)
+
+    print("Root Mean Squared Error (RMSE) on test data = %g" % test_rmse)
+
+    # Save the trained model
+    best_model.write().overwrite().save(output)
 
 if __name__ == '__main__':
     inputs = sys.argv[1]
