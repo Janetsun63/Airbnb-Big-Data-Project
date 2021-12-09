53e0be44125a15451acecdbab9ef8cec3eee6152
William <haonanw@sfu.ca>
2021-12-09 14:35:52 -0800

    Profit prediction

--- src/predict_profit.py
+++ src/predict_profit.py
@@ -8,53 +8,165 @@ import pandas as pd
 
 from pyspark.ml import PipelineModel
 from pyspark.ml.evaluation import RegressionEvaluator
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
 
-# Input: param: dict
-# param = {
-#     'address': str, 
-#     'postcode': str, 
-#     'house_start_date': str/int
-#     'neighbourhood_cleansed': str, 
-#     'room_type': str, 
-#     'accommodates': int, 
-#     'baths': int,  
-#     'beds': int, 
-#     'amenities': int
-# }
-# 
-# Return: Pandas Dataframe
-# | 'price' | 'future_occupancy' | 'profit' |
+spark = SparkSession.builder.appName('Make predictions').getOrCreate()
+assert spark.version >= '3.0' # make sure we have Spark 3.0+
+spark.sparkContext.setLogLevel('WARN')
+sc = spark.sparkContext
 
-# def get_prediction_api(param: dict):
-#     pass
+geocoder = Nominatim(user_agent = 'app_name')
+geocode = RateLimiter(
+    geocoder.geocode, 
+    min_delay_seconds = 1, 
+    return_value_on_exception = None
+)
 
-price_prediction_model = './models/predict_price_GLM'
+# Locations of model and needed dataset
+model_path = './models/predict_price_GLM'
+osm_feature_data_path = './datasets/osm_features/'
 
+# Load OSM data
+osm_feature = spark.read.json(
+    osm_feature_data_path, 
+    schema = types.StructType([
+        types.StructField('osm_id', types.LongType(), nullable = False), 
+        types.StructField('lat', types.DoubleType(), nullable = False), 
+        types.StructField('lon', types.DoubleType(), nullable = False), 
+        types.StructField('category', types.StringType(), nullable = False), 
+        types.StructField('name', types.StringType(), nullable = False)
+    ])
+)
+# Modify column names and cache data
+osm_feature = osm_feature.withColumn('lat_1', osm_feature['lat'])
+osm_feature = osm_feature.withColumn('lon_1', osm_feature['lon'])
+osm_feature = osm_feature[['osm_id', 'lat_1', 'lon_1', 'category', 'name']]
+osm_feature.cache()
+
+# Load price prediction model
+model = PipelineModel.load(model_path)
+
+"""
+Compute distance between any two lat-lon pairs
+Reference: https://stackoverflow.com/questions/639695/how-to-convert-latitude-or-longitude-to-meters
+"""
+def cal_pairwise_dist(lat_lon_paris):
+
+    # An UDF that converts degrees to radians
+    @functions.udf(returnType = types.DoubleType())
+    def deg2Rad(deg):
+        return deg * (np.pi / 180)
+
+    # Radius of Earth in meters
+    R = 6378137
+
+    distances = lat_lon_paris.select(
+        lat_lon_paris['lat_1'], 
+        lat_lon_paris['lon_1'], 
+        lat_lon_paris['category'], 
+        lat_lon_paris['dummy_id'], 
+        lat_lon_paris['lat_2'], 
+        lat_lon_paris['lon_2'], 
+        (deg2Rad(lat_lon_paris['lat_1']) - deg2Rad(lat_lon_paris['lat_2'])).alias('dlat'), 
+        (deg2Rad(lat_lon_paris['lon_1']) - deg2Rad(lat_lon_paris['lon_2'])).alias('dlon')
+    )
+
+    distances = distances.withColumn(
+        'a', 
+        functions.sin(distances['dlat'] / 2) ** 2 \
+        + functions.cos(deg2Rad(distances['lat_1'])) \
+            * functions.cos(deg2Rad(distances['lat_2'])) \
+            * functions.sin(distances['dlon'] / 2) ** 2
+    )
+
+    distances = distances.withColumn(
+        'dist', 
+        2 * R * functions.atan2(
+            functions.sqrt(distances['a']), 
+            functions.sqrt(1 - distances['a'])
+        )
+    )
+
+    distances.cache()
+
+    num_bus_2km = distances.select('dummy_id', 'dist') \
+        .where((distances['category'] == 'bus') & (distances['dist'] <= 2000.0)) \
+        .groupby('dummy_id').agg(functions.count('dist').alias('num_bus_2km'))
+
+    num_subway_2km = distances.select('dummy_id', 'dist') \
+        .where((distances['category'] == 'subway') & (distances['dist'] <= 2000.0)) \
+        .groupby('dummy_id').agg(functions.count('dist').alias('num_subway_2km'))
+
+    shops = distances.select('dummy_id', 'dist') \
+        .where((distances['category'] == 'shopping') & (distances['dist'] <= 5000.0)) \
+        .groupby('dummy_id').agg(
+            functions.count('dist').alias('num_shop_2km'), 
+            functions.min('dist').alias('nearest_shop(m)')
+
+        )
+
+    num_restaurant_2km = distances.select('dummy_id', 'dist') \
+        .where((distances['category'] == 'restaurant') & (distances['dist'] <= 2000.0)) \
+        .groupby('dummy_id').agg(functions.count('dist').alias('num_restaurant_2km'))
+
+    # Result table
+    res_table = num_bus_2km.join(num_subway_2km, on = ['dummy_id']) \
+        .join(shops, on = ['dummy_id']) \
+        .join(num_restaurant_2km, on = ['dummy_id'])
+
+    return res_table
+
+
+"""
+get_prediction_api(dict):
+    Input: param: dict
+    param = {
+        'address': str, 
+        'postcode': str, 
+        'house_start_date': str/int
+        'neighbourhood_cleansed': str, 
+        'room_type': str, 
+        'accommodates': int, 
+        'baths': int,  
+        'beds': int, 
+        'amenities': int
+    }
+
+    Return: Pandas Dataframe
+    | 'price' | 'future_occupancy' | 'profit' |
+"""
 def get_prediction_api(param: dict):
 
-    spark = SparkSession.builder.appName('Make predictions').getOrCreate()
-    assert spark.version >= '3.0' # make sure we have Spark 3.0+
-    spark.sparkContext.setLogLevel('WARN')
-    sc = spark.sparkContext
+    location = geocode(param['address'])
+    lat, lon = location.latitude, location.longitude
 
-    # load the model
-    model = PipelineModel.load(price_prediction_model)
+    if not location:
+        return None
+
+    lat_lon_paris = osm_feature.join(spark.createDataFrame([{
+        'dummy_id': 'dummy_id', 
+        'lat_2': lat, 
+        'lon_2': lon
+    }]))
+    dist = cal_pairwise_dist(lat_lon_paris)
+    dist.cache()
 
     param_df = spark.createDataFrame([{
-        'latitude': 49.2690, 
-        'longitude': -123.139, 
-        'num_bus_2km': 250, 
-        'num_subway_2km': 6, 
-        'num_shop_2km': 140, 
-        'nearest_shop(m)': 70.254, 
-        'num_restaurant_2km': 400, 
-        'neighbourhood_cleansed': 'Fairview', 
-        'room_type': 'Entire home/apt', 
-        'accommodates': 4, 
-        'baths': 4, 
-        'beds': 3, 
-        'days': 1550, 
-        'amenities': 20
+        'latitude': lat, 
+        'longitude': lon, 
+        'num_bus_2km': dist.head(1)[0]['num_bus_2km'], 
+        'num_subway_2km': dist.head(1)[0]['num_subway_2km'], 
+        'num_shop_2km': dist.head(1)[0]['num_shop_2km'], 
+        'nearest_shop(m)': dist.head(1)[0]['nearest_shop(m)'], 
+        'num_restaurant_2km': dist.head(1)[0]['num_restaurant_2km'], 
+        'neighbourhood_cleansed': param['neighbourhood_cleansed'], 
+        'room_type': param['room_type'], 
+        'accommodates': param['accommodates'], 
+        'baths': param['baths'], 
+        'beds': param['beds'], 
+        'days': param['house_start_date'], 
+        'amenities': param['amenities']
     }])
 
     occupancy = spark.createDataFrame(
@@ -68,7 +180,6 @@ def get_prediction_api(param: dict):
     # input_features.show()
 
     predictions = model.transform(input_features)
-
 
     # 1000 values, small enough to proceed with Pandas
     pd_pred = predictions[['future_occupancy', 'prediction']].toPandas()
@@ -90,13 +201,27 @@ def get_prediction_api(param: dict):
     # Compute profit
     pd_pred['profit'] = pd_pred['price'] * pd_pred['future_occupancy'] * 30
 
-    pd_pred.to_csv(
-        './datasets/predictions_tmp.csv', 
-        index = False, 
-        header = True
-    )
+    return pd_pred
 
 
 
 if __name__ == '__main__':
-    get_prediction_api({'a': 1})
+    pd_pred = get_prediction_api(param = {
+        'address': 'Mainland St, Yaletown', 
+        'postcode': 'V6B 5P2', 
+        'house_start_date': 0, 
+        'neighbourhood_cleansed': 'Downtown', 
+        'room_type': 'Entire home/apt', 
+        'accommodates': 2, 
+        'baths': 1, 
+        'beds': 2, 
+        'amenities': 15
+    })
+
+    print(pd_pred)
+
+    # pd_pred.to_csv(
+    #     './datasets/predictions_tmp.csv', 
+    #     index = False, 
+    #     header = True
+    # )
